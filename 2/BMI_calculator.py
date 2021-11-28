97d8ba71fb317fa58b0da30a4a13c67644d7078a
Syket Das <saiketsd23@gmail.com>
2021-11-28 11:43:46 +0530

    bmi calculator

--- 2/BMI_calculator.py
+++ 2/BMI_calculator.py
@@ -4,6 +4,9 @@ height = float(input("Height in meter: "))
 
 bmi = weight / (height ** 2)
 
-print("BMI: ", round(bmi, 2))
+print(f"BMI: {round(bmi, 2)}")  
+
+# using f-String to print the result 
+# round(bmi, 2) = round to 2 decimal places 
 
 #  // ---> floor division (rounds down) int type only 

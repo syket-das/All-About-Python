39ecf85fb3d8771daf6d34f76b4097540149e5d7
Syket Das <saiketsd23@gmail.com>
2021-11-28 11:55:10 +0530

    time left in life

---
+++ 2/time_left_in_your_life.py
@@ -0,0 +1,12 @@
+age_as_int = int(input("How old are you? "))
+
+years_left = 90 - age_as_int
+
+days_left = years_left * 365
+
+weeks_left = years_left * 52
+
+months_left = years_left * 12
+
+print(f"You have {days_left} days \n{weeks_left} weeks \n{months_left} months \n{years_left} years left in your life.")
+
